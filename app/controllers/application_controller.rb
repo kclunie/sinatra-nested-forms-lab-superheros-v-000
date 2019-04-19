6ea3d14373f24b6b220d9ffa994790a8ed2342de
Kristen Clunie <kcluniesa@yahoo.com>
2019-04-19 20:32:37 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,12 +9,17 @@ class App < Sinatra::Base
   end
   
    post '/teams' do
-      @team = Team.new(params[:hero])
-
-      params[:pirate][:ships].each do |details|
-        Ship.new(details)
-      end
-      @ships = Ship.all
+       @team_name = params[:team][:name]
+    @team_motto = params[:team][:motto]
+    @hero_name = []
+    @hero_power = []
+    @hero_bio = []
+    @team_members = params[:team][:members]
+    @team_members.each do |hero|
+      @hero_name << hero[:name]
+      @hero_power << hero[:power]
+      @hero_bio << hero[:bio]
+    end
       
     erb :team
     
